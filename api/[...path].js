0ff3053aab85eed6dda82297c0b3c851ed750141
micmoe <michael.moreliere@gmail.com>
2025-10-08 11:00:27 +0200

    Fix API serverless function handler for Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,5 +1,8 @@
 // Import the Express app from backend
 const app = require('../backend/src/server');
 
-// Export as Vercel serverless function
-module.exports = app;
+// Export as Vercel serverless function handler
+module.exports = (req, res) => {
+  // Forward all requests to the Express app
+  return app(req, res);
+};
